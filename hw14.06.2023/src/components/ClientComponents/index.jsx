2800fc84518351c29efd99601a6e34bbdbee4348
Anahit.Nalbandyan <anahitgrigoryanr@gmail.com>
2023-06-15 21:21:53 +0200

    Developing the code

--- hw14.06.2023/src/components/ClientComponents/index.jsx
+++ hw14.06.2023/src/components/ClientComponents/index.jsx
@@ -1,11 +1,11 @@
 import style from './style.module.css'
 
-export const ClientComponents = ({title, image})=>{
+export const ClientComponents = ({ image})=>{
 
     return(
         <div className={style.clientContainer}>
             <a><img className={style.image} src={image}/></a>
-            <p className={style.title}>{title}</p>
+        
         </div>
     )
 }
